57190d9a387e3d16db236778834939de46d329f4
H2O <h2odevelops@gmail.com>
2021-03-24 16:09:28 -0400

    First commit

---
+++ find-your-hat.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt-sync')({sigint: true});
+
+const hat = '^';
+const hole = 'O';
+const fieldCharacter = '░';
+const pathCharacter = '*';
+
+class Field {
+  constructor(field) {
+    this._field = field;
+  }
+
+  fieldStatus() {
+    console.log(this._field.join("/n"));
+  }
+}
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+myField.fieldStatus();
